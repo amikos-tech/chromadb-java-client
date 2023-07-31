25e8982944a5f9e57d842e8f064e6e6fe99ae791
Trayan Azarov <trayan.azarov@amikos.tech>
2023-08-01 00:01:44 +0300

    feat: Cohere support
    Refs: #1

---
+++ src/main/java/tech/amikos/cohere/CohereClient.java
@@ -0,0 +1,69 @@
+package tech.amikos.cohere;
+
+import com.google.gson.Gson;
+import okhttp3.*;
+
+import java.io.IOException;
+
+public class CohereClient {
+
+    private String baseUrl = "https://api.cohere.ai/";
+
+    private String apiVersion = "v1";
+    private String apiKey;
+
+    private OkHttpClient client = new OkHttpClient();
+    private Gson gson = new Gson();
+    MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    public CohereClient(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+
+    public CohereClient apiKey(String apiKey) {
+        this.apiKey = apiKey;
+        return this;
+    }
+
+    public CohereClient apiVersion(String apiVersion) {
+        this.apiVersion = apiVersion;
+        return this;
+    }
+
+    public CohereClient baseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+        return this;
+    }
+
+    private String getApiKey() {
+        if (this.apiKey == null) {
+            throw new RuntimeException("API Key not set");
+        }
+        return this.apiKey;
+    }
+
+    public CreateEmbeddingResponse createEmbedding(CreateEmbeddingRequest req) {
+        Request request = new Request.Builder()
+                .url(this.baseUrl + apiVersion + "/embed")
+                .post(RequestBody.create(req.json(), JSON))
+                .addHeader("Accept", "application/json")
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Request-Source", "chroma-java-client")
+                .addHeader("Authorization", "Bearer " + getApiKey())
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response);
+            }
+
+            String responseData = response.body().string();
+
+            return gson.fromJson(responseData, CreateEmbeddingResponse.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
--- src/test/java/TestAPI.java
+++ src/test/java/TestAPI.java
@@ -1,9 +1,7 @@
 import com.google.gson.internal.LinkedTreeMap;
 import org.junit.Test;
-import tech.amikos.chromadb.Client;
+import tech.amikos.chromadb.*;
 import tech.amikos.chromadb.Collection;
-import tech.amikos.chromadb.EmbeddingFunction;
-import tech.amikos.chromadb.OpenAIEmbeddingFunction;
 import tech.amikos.chromadb.handler.ApiException;
 
 import java.io.IOException;
@@ -150,5 +148,42 @@ public class TestAPI {
         } catch (ApiException e) {
             assertEquals(e.getCode(), 500);
         }
+    }
+
+    @Test
+    public void testCreateAddCohere() throws ApiException {
+        Utils.loadEnvFile(".env");
+        Client client = new Client(Utils.getEnvOrProperty("CHROMA_URL"));
+        client.reset();
+        String apiKey = Utils.getEnvOrProperty("COHERE_API_KEY");
+        EmbeddingFunction ef = new CohereEmbeddingFunction(apiKey);
+        Collection collection = client.createCollection("test-collection", null, true, ef);
+        List<Map<String, String>> metadata = new ArrayList<>();
+        metadata.add(new HashMap<String, String>() {{
+            put("key", "value");
+        }});
+        Object resp = collection.add(null, metadata, Arrays.asList("Hello, my name is John. I am a Data Scientist."), Arrays.asList("1"));
+        System.out.println(resp);
+        System.out.println(collection.get());
+    }
+
+    @Test
+    public void testQueryExampleCohere() throws ApiException {
+        Utils.loadEnvFile(".env");
+        Client client = new Client(Utils.getEnvOrProperty("CHROMA_URL"));
+        client.reset();
+        String apiKey = Utils.getEnvOrProperty("COHERE_API_KEY");
+        EmbeddingFunction ef = new CohereEmbeddingFunction(apiKey);
+        Collection collection = client.createCollection("test-collection", null, true, ef);
+        List<Map<String, String>> metadata = new ArrayList<>();
+        metadata.add(new HashMap<String, String>() {{
+            put("type", "scientist");
+        }});
+        metadata.add(new HashMap<String, String>() {{
+            put("type", "spy");
+        }});
+        collection.add(null, metadata, Arrays.asList("Hello, my name is John. I am a Data Scientist.", "Hello, my name is Bond. I am a Spy."), Arrays.asList("1", "2"));
+        LinkedTreeMap<String, Object> qr = collection.query(Arrays.asList("Who is the spy"), 10, null, null, null);
+        System.out.println(qr);
     }
 }
